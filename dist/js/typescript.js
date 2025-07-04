f11054efc63eccaa911e94a9e8ab9bd3e9246621
joaomorito <joaomorito04@gmail.com>
2025-07-04 15:13:33 -0300

    aplicando typescript

---
+++ dist/js/typescript.js
@@ -0,0 +1,21 @@
+//Tipos primitivos
+let valor = 3000;
+let nome = "João";
+let isPago = false;
+let qualquer = "";
+qualquer = 22;
+//Arrays
+const lista = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+//Enum
+var TipoTransacaoTeste;
+(function (TipoTransacaoTeste) {
+    TipoTransacaoTeste["DEPOSITO"] = "Dep\u00F3sito";
+    TipoTransacaoTeste["TRANSFERENCIA"] = "Transfer\u00EAncia";
+    TipoTransacaoTeste["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
+})(TipoTransacaoTeste || (TipoTransacaoTeste = {}));
+const novaTransacao = {
+    tipoTransacao: TipoTransacaoTeste.PAGAMENTO_BOLETO,
+    data: new Date(),
+    valor: 0
+};
