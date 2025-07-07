c97f5863dd68b4ca5b9b694d40264acf809acae0
joaomorito <joaomorito04@gmail.com>
2025-07-07 14:24:24 -0300

    Agrupamento de transações por data

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,7 +1,8 @@
 import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
+import { GrupoTransacao } from "./GrupoTransacao.js";
 
-let saldo: number = 3000;
+let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
 const transacaoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
     if (key == "data") {
         return new Date(value);
@@ -19,6 +20,7 @@ function debitar(valor: number): void {
     }
 
     saldo -= valor;
+    localStorage.setItem("saldo", saldo.toString());
 }
 
 function depositar(valor: number): void {
@@ -26,6 +28,7 @@ function depositar(valor: number): void {
         throw new Error("O valor a ser depositado deve ser maior que 0");
     }
     saldo += valor;
+    localStorage.setItem("saldo", saldo.toString());
 }
 
 const Conta = {
@@ -35,6 +38,27 @@ const Conta = {
     
     getDataAcesso() {
         return new Date();
+    },
+
+    getGruposTransacoes(): GrupoTransacao[] {
+        const gruposTransacoes: GrupoTransacao[] = [];
+        const listaTransacoes: Transacao[] = structuredClone(transacaoes);
+        const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+        let labelAtualGrupoTransacao: string = "";
+
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoStransacao: string = transacao.data.toLocaleDateString("pt-br", { month: "long", year: "numeric"});
+            if (labelAtualGrupoTransacao != labelGrupoStransacao) {
+                labelAtualGrupoTransacao = labelGrupoStransacao;
+                gruposTransacoes.push({
+                    label: labelGrupoStransacao,
+                    transacoes: []
+                });
+            }
+            gruposTransacoes.at(-1).transacoes.push(transacao);
+        }
+
+        return gruposTransacoes;
     },
 
     registrarTransacao(novaTransacao: Transacao): void {
@@ -47,7 +71,7 @@ const Conta = {
             }
 
             transacaoes.push(novaTransacao);
-            console.log(novaTransacao);
+            console.log(this.getGruposTransacoes);
             localStorage.setItem("transacoes", JSON.stringify(transacaoes));
     }
 }
